b0650bb3e0b6d8c1d2e30f1b6c06d93f267f6c5f
Ana Simplicio <asimplicio37@gmail.com>
2020-11-20 00:55:11 +0000

    Tela de login

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="form-card">
+        <div class="logo-container">
+            <img class="logo-image" alt="Cetriolo logo" src="../assets/logo.png">
+            <h1 class="logo-text">Cetriolo</h1>
+        </div>
+        <span>Email</span>
+        <input class="form-input"> 
+        <span>Senha</span>
+        <input class="form-input">
+        <button class="form-button">Fazer Login</button>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.form-card {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    background-color: #FFF;
+    padding: 30px 25px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px #91B591;
+    width: 250px;
+}
+
+.form-input {
+    border: transparent;
+    border-bottom: 2px solid #3A663E;
+    height: 20px;
+    padding: 5px;
+    margin-bottom: 20px;
+}
+
+.form-button {
+    font-family: "Roboto";
+    height: 40px;
+    background-color: #194720;
+    color: white;
+    border: transparent;
+    border-radius: 5px;
+}
+
+.logo-container {
+    display: flex;
+    margin: 0 auto;
+    width: 200px;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+.logo-image {
+    height: 50px;
+}
+
+.logo-text {
+    font-family: "Roboto Condensed";
+    font-size: 50px;
+    margin-left: 5px;
+    color:#194720;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login-form />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+      LoginForm
   }
 }
 </script>
+
+<style scoped>
+.home {
+    background-color: #3aaf83;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
